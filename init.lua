04760d54469a84aef6ed94cb6f9980860d85efa0
DjZab <DjZab@users.noreply.github.com>
2018-05-04 13:20:31 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,14 @@
 mymeshnodes = {}
-dofile(minetest.get_modpath("mymeshnodes").."/nodes.lua")
-dofile(minetest.get_modpath("mymeshnodes").."/machine.lua")
-dofile(minetest.get_modpath("mymeshnodes").."/register.lua")
 
-
+if not minetest.get_modpath("moreblocks") or minetest.get_modpath("stairsplus") then
+	dofile(minetest.get_modpath("mymeshnodes").."/defs_full.lua")
+	dofile(minetest.get_modpath("mymeshnodes").."/machine_full.lua")
+	dofile(minetest.get_modpath("mymeshnodes").."/registration_full.lua")
+else
+	dofile(minetest.get_modpath("mymeshnodes").."/defs.lua")
+	dofile(minetest.get_modpath("mymeshnodes").."/machine.lua")
+	dofile(minetest.get_modpath("mymeshnodes").."/registrations.lua")
+end
 
 
 
